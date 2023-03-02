6290046b691ea9924ee158e4a769850fffcb2040
LAPTOP-S0EHIM2P\josec <josecarloscabrera88@gmail.com>
2023-03-02 16:34:47 -0600

    Tarea C#

---
+++ 3. Entrada - Salida/Entrada-Salida/Entrada-Salida/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentos
+{
+    internal class EntradaSalida
+    {
+        public static void Main(string[] args)
+        {
+            string nombre;
+
+            Console.WriteLine("");
+            Console.Write(" Escribe tu nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine(" Hola " + nombre);
+            Console.ReadLine();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 5. Condiciones/Condiciones/Condiciones/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sentencias
+{
+    internal class SentenciaIf
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine(" Ingrese un número: ");
+            int numeroEntero = int.Parse(Console.ReadLine());
+
+            if (numeroEntero == 7)
+                Console.WriteLine(" numeroEntero es 7. ");
+            else if (numeroEntero < 0)
+                Console.WriteLine(" numeroEntero es negativo");
+            else
+                Console.WriteLine(" numeroEntero no es ninguno de los casos programados");
+
+            Console.ReadKey();
+            
+        }
+    }
+}
